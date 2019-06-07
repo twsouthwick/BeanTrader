1cc23c650f8cb0ea47bd58b122fbde53f75c7c6d
Taylor Southwick <tasou@microsoft.com>
2019-06-07 15:39:08 -0700

    Use new WCF configuration system

--- BeanTraderClient/DependencyInjection/BeanTraderServiceClientFactory.cs
+++ BeanTraderClient/DependencyInjection/BeanTraderServiceClientFactory.cs
@@ -1,16 +1,44 @@
-﻿using System.ServiceModel;
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceModel.Configuration;
+using System;
+using System.ServiceModel;
 
 namespace BeanTraderClient.DependencyInjection
 {
-    public class BeanTraderServiceClientFactory
+    public sealed class BeanTraderServiceClientFactory : IDisposable
     {
-        private BeanTraderServiceCallback CallbackHandler { get; }
+        private readonly ServiceProvider provider;
 
         public BeanTraderServiceClientFactory(BeanTraderServiceCallback callbackHandler)
         {
-            CallbackHandler = callbackHandler;
+            provider = CreateServiceProvider(callbackHandler);
         }
 
-        public BeanTraderServiceClient GetServiceClient() => new BeanTraderServiceClient(new InstanceContext(CallbackHandler));
+        public BeanTraderServiceClient GetServiceClient()
+        {
+            return provider.GetRequiredService<BeanTraderServiceClient>();
+        }
+
+        private static ServiceProvider CreateServiceProvider(BeanTraderServiceCallback callback)
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton(callback);
+            services.AddServiceModelClient()
+                .AddConfigurationManagerFile("BeanTraderClient.exe.config");
+            services.AddSingleton<BeanTraderServiceClient>();
+
+            return services.BuildServiceProvider();
+        }
+
+        public void Dispose() => provider.Dispose();
+    }
+}
+
+public partial class BeanTraderServiceClient
+{
+    public BeanTraderServiceClient(BeanTraderServiceCallback callback, IChannelFactoryProvider provider)
+        : base(new InstanceContext(callback), provider.GetEndpoint<BeanTraderService>())
+    {
     }
 }
